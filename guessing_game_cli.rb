654ddc45742817e9f958f6f51b7d2e06f1215840
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-04 20:35:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   user_guess = gets.chomp
   
   if user_guess.to_i == computer_guess
-    puts "You guessed correct number!"
+    puts "You guessed the correct number!"
   elsif user_guess.to_i != computer_guess and user_guess != "exit"
     puts "Sorry! The computer guessed #{computer_guess}."
   else user_guess == "exit"
