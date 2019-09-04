5d25f3a608a0907eccd2ba7192080460e1d4df30
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-04 20:30:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  computer_guess = rand(1..7)
+  computer_guess = rand(1..6)
   user_guess = gets.chomp
   
   if user_guess.to_i == computer_guess
